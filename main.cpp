81183bcb10c08b796f8c1637ecb80e6c9ffa68b2
Anastasia1151 <111795374+Anastasia1151@users.noreply.github.com>
2022-08-28 18:40:52 +0300

    добавила функцию, аналогичную strchr

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int my_strlen(char line[]);
 void my_strcpy(char str[], char line[]);
+void my_strchr(char line[], int symbol);
 
 int main()
 {
@@ -11,7 +12,7 @@ int main()
     printf("Введите строку 1:\n");
     scanf("%s", str);
     int len = my_strlen(str);
-    printf("Длина строки равна %d\n", len);
+    printf("Длина строки равна %d\n\n", len);
 
     printf("Введите строку 2:\n");
     scanf("%s", line);
@@ -19,6 +20,7 @@ int main()
     printf("Длина строки равна %d\n", len);
 
     my_strcpy(str, line);
+    my_strchr(line, '6');
 
     return 0;
 }
@@ -33,9 +35,16 @@ int my_strlen(char line[]){
 
 void my_strcpy(char str[], char line[]){
     int i = 0;
-    for ( ; str[i] != '\0'; i++ ){
+    for ( ; line[i] != '\0'; i++ ){
         str[i] = line[i];
     }
     str[i] = '\0';
-    printf("Изменённая строка: %s\n", str);
+    printf("Изменённая строка: %s\n\n", str);
+}
+
+void my_strchr(char line[], int symbol){
+    int i = 0;
+    for ( ; line[i] != '\0'; i++){
+        if ( line[i] == symbol ) printf("Искомый символ находится в строке 2 на позиции %d\n", i + 1);
+    }
 }
