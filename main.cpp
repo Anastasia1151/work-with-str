ed577dd9941f6d0d6c78551c086f4cd93483c721
Anastasia1151 <111795374+Anastasia1151@users.noreply.github.com>
2022-08-28 16:57:23 +0300

    Добавила функцию, аналогичную strcpy

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int my_strlen(char line[]);
+void my_strcpy(char str[], char line[]);
+
 int main()
 {
-    char line[] = "";
+    char line[20] = "";
+    char str[20] = "";
 
-    printf("Введите строку:\n");
-    scanf("%s", &line);
-    int len = my_strlen(line);
+    printf("Введите строку 1:\n");
+    scanf("%s", str);
+    int len = my_strlen(str);
     printf("Длина строки равна %d\n", len);
+
+    printf("Введите строку 2:\n");
+    scanf("%s", line);
+    len = my_strlen(line);
+    printf("Длина строки равна %d\n", len);
+
+    my_strcpy(str, line);
 
     return 0;
 }
@@ -19,4 +29,13 @@ int my_strlen(char line[]){
         i++;
     }
     return i;
+}
+
+void my_strcpy(char str[], char line[]){
+    int i = 0;
+    for ( ; str[i] != '\0'; i++ ){
+        str[i] = line[i];
+    }
+    str[i] = '\0';
+    printf("Изменённая строка: %s\n", str);
 }
